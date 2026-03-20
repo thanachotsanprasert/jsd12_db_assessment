5a3a72d8f84daade416ede542d4bf6dfa732a755
thanacohtsanprasert <t.sprst.work@gmail.com>
2026-03-20 15:10:57 +0700

    sql task4

--- jsd12-database-assessment/02_querying_assessment/postgresql/query_task4.sql
+++ jsd12-database-assessment/02_querying_assessment/postgresql/query_task4.sql
@@ -10,6 +10,35 @@
 
 -- ---------------------------------------------------------------
 -- Your thinking process (required)
+/*
+1 โจทย์ให้เราหาว่าถ้า Freshest Farm Produce ไม่มาส่งของ
+2 เราต้องดึงข้อมูล ingredients ออกมาดูก่อน ว่า Freshest Farm Produce ส่งอะไรให้เราบ้าง
+
+SELECT *
+FROM Ingredients
+
+3 จากด้านบนเราจะได้ ingredients ที่มี supplier_id เราจะ JOIN ตารางกับ supplier db
+
+SELECT 
+  i.name AS ingredient_name, ** เราจะเป็นต้องเปลี่ยนชื่อเพราะ column มีชื่อเดียวกัน
+  s.name AS supplier_name ** เราจะเป็นต้องเปลี่ยนชื่อเพราะ column มีชื่อเดียวกัน
+FROM Ingredients i
+JOIN Suppliers s USING (supplier_id);
+
+จะเห็นได้ว่าจาก * ต้องเปลี่ยนเป็นการสั่งตารางเฉพาะ เพราะเป็นการสั่งให้ sql นำตารางไหน มารวมกัน
+
+4 จาก code ด้านบนเราจะ filter ตารางโดยใข้ WHERE (เพิ่ม WHERE s.name = 'Freshest Farm Produce';)
+
+SELECT 
+  i.name AS ingredient_name,
+  s.name AS supplier_name
+FROM Ingredients i
+JOIN Suppliers s USING (supplier_id)
+WHERE s.name = 'Freshest Farm Produce';
+
+5 code ด้านจะบอกเราว่า Freshest Farm Produce ขายอะไรให้เราบ้าง แล้วนี่คือคำตอบ
+
+*/
 -- ---------------------------------------------------------------
 -- Before writing your query, explain in your own words how you
 -- interpreted the task, what data you need, which table(s) are
@@ -18,3 +47,10 @@
 --
 -- Your thinking:
 --
+
+SELECT 
+  i.name AS ingredient_name,
+  s.name AS supplier_name
+FROM Ingredients i
+JOIN Suppliers s USING (supplier_id)
+WHERE s.name = 'Freshest Farm Produce';
